e871e7be1627549ea5364d896f5fabd85543c88e
Crowdbotics <team@crowdbotics.com>
2020-12-29 13:59:17 +0000

    Update components

--- src/features/BlankScreen016471/screens/blank.js
+++ src/features/BlankScreen016471/screens/blank.js
@@ -20,15 +20,34 @@ export class Blank extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
   }
-  state = {}
+  state = { DateTimePicker_3: new Date(""), DateTimePicker_4: new Date("") }
   render = () => (
     <View>
       <Text>Sample text content</Text>
+      <DateTimePicker
+        showIcon={false}
+        date={this.state.DateTimePicker_3}
+        onDateChange={selectedDate =>
+          this.setState({ DateTimePicker_3: selectedDate })
+        }
+      />
+      <DateTimePicker
+        showIcon={false}
+        date={this.state.DateTimePicker_4}
+        onDateChange={selectedDate =>
+          this.setState({ DateTimePicker_4: selectedDate })
+        }
+      />
     </View>
   )
 }
 
-const styles = StyleSheet.create({ View_1: {}, Text_2: {} })
+const styles = StyleSheet.create({
+  View_1: {},
+  Text_2: {},
+  DateTimePicker_3: {},
+  DateTimePicker_4: {}
+})
 
 function mapStateToProps(state) {
   return { state: state.apiReducer }
